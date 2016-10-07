6a22c0c7e0fa4232712b5794b0594c46d63dc33f
Ronda Bergman <rondabergman@hotmail.com>
2016-10-07 15:05:42 -0400

    Added comments

--- FridaySample/FridaySample/Program.cs
+++ FridaySample/FridaySample/Program.cs
@@ -10,25 +10,38 @@ namespace FridaySample
     {
         static void Main(string[] args)
         {
+            //Set the initial value to true so the while loop executes at least once
             bool addAnother = true;
+
+            //Create the list to hold all the cool things
             List<SomeCoolThing> coolList = new List<SomeCoolThing>();
 
+            //While addAnother is true do all this again
             while(addAnother)
             {
+                //Create the cool thing object
                 SomeCoolThing theThing = new SomeCoolThing();
 
+                //Ask the user for the value
                 Console.WriteLine("What is the value of the first property?");
+                //Read the value into a temporary variable
                 string response = Console.ReadLine();
+                //Put the user's response into the object
                 theThing.MyProperty1 = response;
 
+                //Ask the user for the value
                 Console.WriteLine("What is the value of the second property?");
+                //Read the value into a temporary variable
                 string anotherResponse = Console.ReadLine();
-
+                //Put the user's response into the object
                 theThing.MyProperty2 = anotherResponse;
 
+                //Add the object to the list
                 coolList.Add(theThing);
 
+                //Determine if the user wants to do it all again
                 Console.WriteLine("Would you like to create another cool thing? (y/n)");
+                //Get the user's response
                 string answer = Console.ReadLine();
 
                 if (answer == "y")
@@ -41,9 +54,10 @@ namespace FridaySample
                 }
             }
 
+            //Now that we are done looping to get the user's response, write out the things
             for(int i = 0; i < coolList.Count; i++)
             {
-                Console.WriteLine(string.Format("My Cool thing {0} property1 = {1}, property = {2}", 
+                Console.WriteLine(string.Format("My Cool thing {0} property1 = {1}, property = {2}",
                     i, coolList[i].MyProperty1, coolList[i].MyProperty2));
             }
 
